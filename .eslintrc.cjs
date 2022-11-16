ace3b8fc71c653ced3fbc9c8e886bf442e11e8d9
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-11-16 13:46:27 -0300

    Installing ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'prefer-default-export': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-no-bind': 0,
+    '@typescript-eslint/no-unused-vars': 1,
+    'react/no-unstable-nested-components': 0,
+
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0,
+    'jsx-a11y/alt-text': 0,
+    'no-restricted-globals': 1,
+    'no-unused-vars': 1,
+
+    'react/require-default-props': 0,
+  },
+};
